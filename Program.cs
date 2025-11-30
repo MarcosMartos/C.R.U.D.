5465c5bdd15169214d05b7baad397c12bdddc1ef
Marcos <marcosmartostuvi@gmail.com>
2025-11-30 19:46:16 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ 3. **Mini sistema CRUD simple (sin base de datos real)**
+    - Implementar un menú en consola con opciones como “Agregar cliente”,
+    “Listar clientes”, “Modificar cliente”, “Eliminar cliente”, “Guardar cambios en archivo”. 
+    Esto te obliga a pensar en estructura de datos, persistencia simple y lógica.
+ */
+
+class CRUD
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hola");
+    }
+
+
+
+
+}
