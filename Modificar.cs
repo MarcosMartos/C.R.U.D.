e7de6265f665fd8a6dfc35794d15435c882bfded
Marcos <marcosmartostuvi@gmail.com>
2025-12-03 14:41:19 -0300

    version beta terminada

--- Modificar.cs
+++ Modificar.cs
@@ -120,7 +120,7 @@ class Modificar
             {
                 try
                 {
-                    Console.WriteLine("Escriba la nueva descripción.");
+                    Console.WriteLine("Escriba la nueva categoría.");
                     gastoEncontrado.categoria = Console.ReadLine();
 
                     Console.WriteLine("Categoría modificada correctamente.");
@@ -174,7 +174,7 @@ class Modificar
             {
                 try
                 {
-                    Console.WriteLine("Escriba la nueva descripción.");
+                    Console.WriteLine("Escriba el nuevo monto.");
                     gastoEncontrado.monto = int.Parse(Console.ReadLine());
 
                     Console.WriteLine("Monto modificado correctamente.");
@@ -203,7 +203,53 @@ class Modificar
     // Funcion para modificar el vencimiento
     public void modificarVencimiento()
     {
-        Console.WriteLine("Vencimiento");
+        // Buscamos el archivo en la ruta
+        string rutaData = "C:\\Users\\Marcos\\source\\repos\\CRUD\\data";
+        string archivo = Path.Combine(rutaData, "gastos.json");
+
+        List<Gastos> gastos = new List<Gastos>();
+
+        // Verificamos que existe o lo creamos
+        if (File.Exists(archivo))
+        {
+            var opciones = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            string json = File.ReadAllText(archivo);
+            gastos = JsonSerializer.Deserialize<List<Gastos>>(json, opciones) ?? new List<Gastos>();
+
+            Console.WriteLine("Ingrese ID del elemento a modificar: ");
+            int idElemento = int.Parse(Console.ReadLine());
+
+            var gastoEncontrado = gastos.FirstOrDefault(g => g.id == idElemento);
+
+            if (gastoEncontrado != null)
+            {
+                try
+                {
+                    Console.WriteLine("Escriba la nueva fecha de vencimiento (formato: dd/mm/aaaa): ");
+                    gastoEncontrado.vencimiento = DateTime.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Vencimiento modificado correctamente.");
+                }
+                catch
+                {
+                    Console.WriteLine("Error al cambiar la fecha de vencimiento.");
+                }
+
+                string archivoJson = JsonSerializer.Serialize(gastos, new JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText(archivo, archivoJson);
+            }
+            else
+            {
+                Console.WriteLine("No se encontró ese ID.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("No se encuentra un archivo de gastos.");
+        }
 
     }
 
