832c132dbbe2b6afcdc5a84625ec3ebe6a17cf38
Marcos <marcosmartostuvi@gmail.com>
2025-12-03 15:17:08 -0300

    separar capas, estructura del proyecto

---
+++ models/Gastos.cs
@@ -0,0 +1,11 @@
+﻿// Creamos una clase Gastos con sus atributos
+class Gastos
+{
+    public int id { get; set; }
+    public string descripcion { get; set; }
+    public string categoria { get; set; }
+    public int monto { get; set; }
+    public DateTime vencimiento { get; set; }
+    public bool estado { get; set; }
+
+}
--- ui/Menu.cs
+++ ui/Menu.cs
@@ -41,17 +41,6 @@ class Menu
     }
 }
 
-// Creamos una clase Gastos con sus atributos
-class Gastos
-{
-    public int id { get; set; }
-    public string descripcion { get; set; }
-    public string categoria { get; set; }
-    public int monto { get; set; }
-    public DateTime vencimiento { get; set; }
-    public bool estado { get; set; }
-
-}
 
 
 
